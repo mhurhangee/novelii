f4c6ed4ff3022959b7984af84dc718c30e04314d
Michael Hurhangee <m.hurhangee@me.com>
2025-07-08 14:44:36 +0000

    lint

--- lib/config/ai/models.ts
+++ lib/config/ai/models.ts
@@ -1,7 +1,7 @@
-import { openai } from '@ai-sdk/openai'
 import { groq } from '@ai-sdk/groq'
+import { openai } from '@ai-sdk/openai'
 
 export const models = {
   mini: openai('gpt-4o-mini'),
-  groq: groq('gemma2-9b-it')
+  groq: groq('gemma2-9b-it'),
 }
